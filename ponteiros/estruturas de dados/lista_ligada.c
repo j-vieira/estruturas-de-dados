dd06787e031a179a164ad8eae2268314eb455cd5
Joao Vitor <jvsantospessoal@gmail.com>
2025-04-13 14:38:55 -0300

    Consertando bugs da lista ligada.

--- ponteiros/estruturas de dados/lista_ligada.c
+++ ponteiros/estruturas de dados/lista_ligada.c
@@ -2,47 +2,45 @@
 #include <stdlib.h>
 
 typedef struct No {
-	int valor;
-	struct No *proximo;
+    int valor;
+    struct No *proximo;
 } No;
 
 No *percorrerLista(No *no) {
-	if (no->proximo == NULL) {
-		return no;
-	}
-	printf("%d ", no->valor);
-	return percorrerLista(no);
+    if (no->proximo == NULL) {
+        return no; 
+    }
+    return percorrerLista(no->proximo);  
 }
 
 No *adicionar(No *no, int valor) {
-	No *novoNo = malloc(sizeof(No));
-	novoNo->valor = valor;
-	novoNo->proximo = NULL;
+    No *novoNo = malloc(sizeof(No));  
+    novoNo->valor = valor;
+    novoNo->proximo = NULL;
 
-	No *ultimoNoAtual = percorrerLista(no);
-
-	ultimoNoAtual->proximo = novoNo;
-	return novoNo;
+    No *ultimoNoAtual = percorrerLista(no);  
+    ultimoNoAtual->proximo = novoNo;  
+    return novoNo;  
 }
 
-
 void lerLista(No *no) {
-	printf("%d ", no->valor);
-	if (no->proximo == NULL) {
-		return;
-	}
-	return lerLista(no->proximo);
+    printf("%d ", no->valor);
+    if (no->proximo != NULL) {
+        lerLista(no->proximo);  
+    }
 }
 
 int main() {
-	int capacidade = 10;
+    int capacidade = 10;
 
-	No *inicio = malloc(capacidade * sizeof(No));
-	inicio->valor = 10;
-	inicio->proximo = NULL;
+    No *inicio = malloc(sizeof(No));  
+    inicio->valor = 1;
+    inicio->proximo = NULL;
 
-	No *novoNo = adicionar(inicio, 5);
-	adicionar(novoNo, 15);
+    adicionar(inicio, 2);
+    adicionar(inicio, 3);
+    adicionar(inicio, 4);
 
-	lerLista(inicio);
+    lerLista(inicio);
+    return 0;
 }
